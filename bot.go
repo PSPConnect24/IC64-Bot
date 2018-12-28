a6094c0556213c21605c492e252d354387b36458
Marko <32610623+Apfel@users.noreply.github.com>
2018-12-29 00:25:04 +0100

    Added basic source

---
+++ bot.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/bwmarrin/discordgo"
+)
+
+func main() {
+	config := Load()
+	client, err := discordgo.New("Bot " + config.Bot.Token)
+	if err != nil {
+		fmt.Println("An error occurred when initializing the Client: ", err)
+		return
+	}
+	client.AddHandler(ready)
+	err = client.Open()
+	if err != nil {
+		fmt.Println("An error occurred when initializing the connection: ", err)
+	}
+	sc := make(chan os.Signal, 1)
+	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt, os.Kill)
+	<-sc
+	client.Close()
+}
+
+func ready(session *discordgo.Session, event *discordgo.Ready) {
+	session.UpdateStatus(0, "Apparently I am a thing now")
+	fmt.Println("Bot is now ready.")
+}
---
+++ config.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"encoding/xml"
+	"fmt"
+	"io/ioutil"
+	"os"
+)
+
+// Config - Overall configuration
+type Config struct {
+	XMLName xml.Name  `xml:"config"`
+	Bot     BotConfig `xml:"bot"`
+}
+
+// BotConfig - Bot-specific settings
+type BotConfig struct {
+	Token  string `xml:"token"`
+	Prefix string `xml:"prefix"`
+}
+
+// Load - Loads the configuration into memory
+func Load() Config {
+	xmlFile, err := os.Open("config.xml")
+	if err != nil {
+		fmt.Println("An error occurred!\n", err)
+	}
+	defer xmlFile.Close()
+	data, _ := ioutil.ReadAll(xmlFile)
+	var config Config
+	xml.Unmarshal(data, config)
+	return config
+}
