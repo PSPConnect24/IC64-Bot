bd2a798016b9f878aee9ad4f274fc622255c6cb1
Apfel <32610623+Apfel@users.noreply.github.com>
2019-01-18 20:28:36 +0100

    big oops

--- commands/ping.go
+++ commands/ping.go
@@ -11,7 +11,7 @@ import (
 // Ping command.
 func Ping(ctx anpan.Context, _ []string) {
 	ping, _ := ctx.Reply("Let me check that for you!")
-	d, _ := time.ParseDuration("1ms")
+	d, _ := time.ParseDuration("300ms")
 	tsOne, _ := ping.Timestamp.Parse()
 	took := time.Now().Sub(tsOne).Round(d).String()
 
@@ -20,5 +20,5 @@ func Ping(ctx anpan.Context, _ []string) {
 		Description: fmt.Sprintf(":ping_pong: Pong! Ping: **%s**!", took),
 	}
 
-	ctx.Session.ChannelMessageEditEmbed(ctx.Message.ChannelID, ping.ID, embed)
+	ctx.Session.ChannelMessageEditEmbed(ctx.Channel.ID, ping.ID, embed)
 }
