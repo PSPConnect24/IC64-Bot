29c5e5402012be0dbbcbccbe16621aa7c1c5f867
Apfel <32610623+Apfel@users.noreply.github.com>
2019-01-18 20:12:29 +0100

    fixed ping command and added relevant information for IC64

--- commands/about.go
+++ commands/about.go
@@ -10,8 +10,13 @@ func About(ctx anpan.Context, _ []string) {
 	ctx.ReplyEmbed(&discordgo.MessageEmbed{
 		Title:       "About IronConnect64-Bot",
 		Description: "IronConnect64-Bot is a bot made specifically for IronConnect64, made to give you related information and content for IronConnect64.",
+		URL:         "https://ic64.xyz/",
 		Thumbnail: &discordgo.MessageEmbedThumbnail{
 			URL: ctx.Session.State.User.AvatarURL(""),
 		},
+		Fields: []*discordgo.MessageEmbedField{&discordgo.MessageEmbedField{
+			Name:  "Related content",
+			Value: "**[Website](ic64.xyz)**\n**[IC64 on GitHub](https://github.com/IronConnect64)**\n**[IC64-Bot's repository](https://github.com/IronConnect64/IC64-Bot)**",
+		}},
 	})
 }
