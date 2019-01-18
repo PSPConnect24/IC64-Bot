fa8dbcf3ee76572e477f1a99edca107530e0ef32
Apfel <32610623+Apfel@users.noreply.github.com>
2019-01-18 20:34:48 +0100

    Apfel can do the maths

--- commands/ping.go
+++ commands/ping.go
@@ -11,13 +11,12 @@ import (
 // Ping command.
 func Ping(ctx anpan.Context, _ []string) {
 	ping, _ := ctx.Reply("Let me check that for you!")
-	d, _ := time.ParseDuration("300ms")
 	tsOne, _ := ping.Timestamp.Parse()
-	took := time.Now().Sub(tsOne).Round(d).String()
+	took := time.Now().Sub(tsOne).Seconds() * float64(1000)
 
 	embed := &discordgo.MessageEmbed{
 		Title:       "Pong!",
-		Description: fmt.Sprintf(":ping_pong: Pong! Ping: **%s**!", took),
+		Description: fmt.Sprintf(":ping_pong: Pong! Ping: **%f**!", took),
 	}
 
 	ctx.Session.ChannelMessageEditEmbed(ctx.Channel.ID, ping.ID, embed)
