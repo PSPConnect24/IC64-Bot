99301a953b14add4a8bda1fab92aa84538f0b01d
Apfel <32610623+Apfel@users.noreply.github.com>
2019-01-19 23:11:53 +0100

    some epic changes

--- bot.go
+++ bot.go
@@ -17,11 +17,9 @@ func main() {
 	if err != nil {
 		log.Fatalf("An error occurred when initializing the Client: %s", err.Error())
 	}
-
 	handler := anpan.NewCommandHandler(config.Prefixes, config.Owners, true, true)
 	handler.StatusHandler.SetSwitchInterval("30s")
 	handler.StatusHandler.AddEntry("IC64-Bot")
-
 	handler.AddCommand("about", "Gives you information about the bot.", false, false, 0, commands.About)
 	handler.AddCommand("eval", "", true, true, 0, commands.Eval)
 	handler.AddCommand("ping", "Check the bot's ping.", false, false, 0, commands.Ping)
@@ -31,17 +29,13 @@ func main() {
 
 	err = client.Open()
 	if err != nil {
-		log.Fatalf("An error occurred when initializing the connection: %s", err)
+		log.Fatalf("An error occurred when initializing the connection: %s", err.Error())
 	}
-
-	handler.AddPrefix(client.State.User.Mention())
-
+	handler.AddPrefix(client.State.User.Mention() + " ")
 	log.Printf("Client running, logged in as %s#%s (ID: %s)\n", client.State.User.Username, client.State.User.Discriminator, client.State.User.ID)
-
 	sc := make(chan os.Signal, 1)
 	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt, os.Kill)
 	<-sc
-
 	client.Close()
 	log.Fatalln("Caught shutdown signal, shutting down...")
 }
