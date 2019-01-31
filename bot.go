de0b8cfbc3325691984c11aca0bbe7792b22f905
Apfel <32610623+Apfel@users.noreply.github.com>
2019-01-31 19:55:10 +0100

    Please tell me this fixes it

--- bot.go
+++ bot.go
@@ -17,13 +17,14 @@ func main() {
 	if err != nil {
 		log.Fatalf("An error occurred when initializing the Client: %s", err.Error())
 	}
+
 	handler := anpan.NewCommandHandler(config.Prefixes, config.Owners, true, true)
-	handler.StatusHandler.SetSwitchInterval("30s")
-	handler.StatusHandler.AddEntry("IC64-Bot")
+
 	handler.AddCommand("about", "Gives you information about the bot.", false, false, 0, commands.About)
 	handler.AddCommand("eval", "", true, true, 0, commands.Eval)
 	handler.AddCommand("ping", "Check the bot's ping.", false, false, 0, commands.Ping)
 	handler.AddCommand("shutdown", "", true, true, 0, commands.Shutdown)
+
 	client.AddHandler(handler.OnMessage)
 	client.AddHandler(handler.StatusHandler.OnReady)
 
@@ -31,7 +32,7 @@ func main() {
 	if err != nil {
 		log.Fatalf("An error occurred when initializing the connection: %s", err.Error())
 	}
-	handler.AddPrefix(client.State.User.Mention() + " ")
+
 	log.Printf("Client running, logged in as %s#%s (ID: %s)\n", client.State.User.Username, client.State.User.Discriminator, client.State.User.ID)
 	sc := make(chan os.Signal, 1)
 	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt, os.Kill)
