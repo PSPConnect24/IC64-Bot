deb7d01c1cc414418b4f62d8cc0469f14c0feec1
Apfel <32610623+Apfel@users.noreply.github.com>
2019-02-13 20:05:05 +0100

    Added basic tag command

---
+++ cmd-tag.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"strings"
+
+	gotag "github.com/Apfel/GOtag"
+	gotaglib "github.com/Apfel/GOtag/libraries"
+	"github.com/MikeModder/anpan"
+)
+
+var parser = gotag.NewBuilder().AddMethods(gotaglib.Arguments()).Build()
+
+func tagcmd(ctx *anpan.Context, args []string) {
+	ctx.Reply(parser.Parse(strings.Join(args, " ")))
+}
