c00f05c5b39c58880c0c6fa983a9479bc944bd53
Apfel <32610623+Apfel@users.noreply.github.com>
2019-02-17 16:36:47 +0100

    import every default library, thanks

--- cmd-tag.go
+++ cmd-tag.go
@@ -8,7 +8,7 @@ import (
 	"github.com/MikeModder/anpan"
 )
 
-var parser = gotag.NewBuilder().AddMethods(gotaglib.Arguments()).Build()
+var parser = gotag.NewBuilder().AddMethods(gotaglib.Arguments()).AddMethods(gotaglib.Functional()).AddMethods(gotaglib.Miscellaneous()).AddMethods(gotaglib.Strings()).AddMethods(gotaglib.Time()).AddMethods(gotaglib.Variables()).Build()
 
 func tagcmd(ctx anpan.Context, args []string) {
 	ctx.Reply(parser.Parse(strings.Join(args, " ")))
