d425c8b22201eedc177f9f2423f7c82bb071e618
Apfel <32610623+Apfel@users.noreply.github.com>
2019-02-20 17:14:22 +0100

    YAML is fun

--- config.go
+++ config.go
@@ -1,27 +1,28 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
 	"os"
+
+	"gopkg.in/yaml.v2"
 )
 
 type config struct {
-	Token    string   `json:"token"`
-	Prefixes []string `json:"prefixes"`
-	Owners   []string `json:"owners"`
+	Token    string   `yaml:"token"`
+	Prefixes []string `yaml:"prefixes"`
+	Owners   []string `yaml:"owners"`
 }
 
 func load() config {
 	var conf config
-	cfg, err := os.Open("config.json")
+	cfg, err := os.Open("config.yml")
 	if err != nil {
-		fmt.Println("Failed to open config.json: ", err)
+		fmt.Println("Failed to open config.yml: ", err)
 		os.Exit(1)
 	}
-	err = json.NewDecoder(cfg).Decode(&conf)
+	err = yaml.NewDecoder(cfg).Decode(&conf)
 	if err != nil {
-		fmt.Println("Failed to decode config.json: ", err)
+		fmt.Println("Failed to decode config.yml: ", err)
 		os.Exit(1)
 	}
 	cfg.Close()
